088556ac4289c66f22a12c12291b1026aa40b586
Денис Пономарев <deskin196@gmail.com>
2023-07-08 19:50:02 +0300

    Написал метод возвращающий строку с всем справочником отсортированным по убыванию кол-ва номеров

--- TelephoneBook.java
+++ TelephoneBook.java
@@ -1,5 +1,15 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import javax.swing.text.html.parser.Entity;
 
 public class TelephoneBook{
 
@@ -30,8 +40,29 @@ public class TelephoneBook{
             map.get(name).removeIf(value -> value == number);
         }
     }
+    //Возвращает список телефонов записи по фамилии
     public ArrayList<Integer> getTelephoneNumber(String name){
         return map.get(name);
+    }
+
+    public String getAllTelephoneBook(){
+        StringBuilder stringBuilder = new StringBuilder();
+        //Создаем компаратор и присваиваем ему лямбда выражение с алгоритмом сравнения, он принимает два списка и возвращает 0 если они равны
+        // отрицательное число если второй меньше и положительное если второй больше
+        Comparator<ArrayList<Integer>> bySize = ( ArrayList<Integer> obj1, ArrayList<Integer> obj2) -> {
+           return  obj2.size() - obj1.size();
+        };
+
+        //Передаем компаратор в сортировку и получаем отсортированный Затем с помощью forEach и переданной в него лямбды наполняем stringBuilder 
+        map.entrySet().stream().sorted(Map.Entry.<String, ArrayList<Integer>>comparingByValue(bySize)).collect(Collectors.toList()).
+        forEach(p->{
+            stringBuilder.append(p.getKey());
+            stringBuilder.append(p.getValue().toString());
+            stringBuilder.append("\n");
+        });
+        return stringBuilder.toString();
+
+        
     }
 
     public String toString(){
